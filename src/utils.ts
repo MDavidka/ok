1b40a15b50884e683950a6d06473e05588fdfb15
Márton I. Dávid <117516603+MDavidka@users.noreply.github.com>
2026-03-07 18:04:51 +0200

    Deploy from Sycord AI Builder (Clean Re-deploy)

--- src/utils.ts
+++
@@ -1,9 +0,0 @@
-import { ComponentProps } from './types';
-
-export function classNames(...classes: (string | undefined | null)[]): string {
-  return classes.filter(Boolean).join(' ');
-}
-
-export function createProps(className: string | undefined): ComponentProps {
-  return { className };
-}
